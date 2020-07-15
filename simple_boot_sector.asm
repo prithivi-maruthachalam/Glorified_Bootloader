9bc14083baa78f7f35ae0fc77394163355336ee1
prythagoras <prithivimaruthachalam@gmail.com>
2020-07-15 22:05:17 +0530

    Bootsector prints characters

--- simple_boot_sector.asm
+++ simple_boot_sector.asm
@@ -1,9 +1,14 @@
-;putting in an infinite loop
-loop:
-    jmp loop
+;setup tty mode 
+mov ah, 0x0e ;put int 10 into the high bit of the ax register
 
-;padding wit 510 - size of code so far - zeroes
+mov al, 'H' ;Move the character to be printed into the lowest bit of the ax register
+int 0x10 ;call the interrupt to print on the screen
+mov al, 'E'
+int 0x10
+
+;putting in an infinite loop for persistence
+jmp $
+
+;padding with 510 - size of code so far - zeroes and magic number to ID as bootsector code
 times 510 - ($-$$) db 0
-
-;number to indicate that this is a boot sector file
 dw 0xaa55
