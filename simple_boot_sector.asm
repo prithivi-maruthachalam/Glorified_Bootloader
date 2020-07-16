1b8c53818500bc15c23e89e7d02f28883f11bb4f
prythagoras <prithivimaruthachalam@gmail.com>
2020-07-16 09:39:20 +0530

    Wrote string printing function

--- simple_boot_sector.asm
+++ simple_boot_sector.asm
@@ -1,13 +1,26 @@
-;setup tty mode 
-mov ah, 0x0e ;put int 10 into the high bit of the ax register
+[org 0x7c00]
 
-mov al, 'H' ;Move the character to be printed into the lowest bit of the ax register
-int 0x10 ;call the interrupt to print on the screen
-mov al, 'E'
-int 0x10
+mov bx, HELLO_STRING
+call print_string
+
+call print_newline
+
+mov bx, GOODBYE_STRING
+call print_string
+
 
 ;putting in an infinite loop for persistence
 jmp $
+
+;inlcuding files
+%include "print_string.asm"
+
+;data
+HELLO_STRING:
+    db 'Hello World',0
+GOODBYE_STRING:
+    db 'Goodbye World',0
+
 
 ;padding with 510 - size of code so far - zeroes and magic number to ID as bootsector code
 times 510 - ($-$$) db 0
