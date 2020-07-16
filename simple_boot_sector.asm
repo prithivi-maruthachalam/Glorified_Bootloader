0b047dee957af849c1bbce5f2e810e91928db515
prythagoras <prithivimaruthachalam@gmail.com>
2020-07-16 16:58:19 +0530

    Created disk reading function

--- simple_boot_sector.asm
+++ simple_boot_sector.asm
@@ -1,16 +1,26 @@
-[org 0x7c00]
+[org 0x7c00] ;offeset from the bootloader's current position in memory
 
-mov bx, HELLO_STRING
+;main greeting
+mov bx, GREETING_STRING
 call print_string
 
 call print_newline
+call print_newline
 
-mov bx, GOODBYE_STRING
-call print_string
+;setting up stack at a far away point from current point
+mov bp, 0x8000
+mov sp,bp ;moving start to base
+
+mov bx, 0x9000 ;that's the location we want to read to with an es offset, which is set by default
+mov dh, 2 ;number of sectors to read - parameter for disk_load
+call disk_load
+
+mov dx, [0x9000] ;retrieve the frist word and put it in the parameter for print_hex
+call print_hex_data
 
 call print_newline
 
-mov dx, 0x123A
+mov dx, [0x9000 + 512] ;first word loaded from second sector
 call print_hex_data
 
 
@@ -19,14 +29,19 @@ jmp $
 
 ;inlcuding files
 %include "printing.asm"
+%include "disk_access.asm"
 
 ;data
-HELLO_STRING:
-    db 'Hello World',0
-GOODBYE_STRING:
-    db 'Goodbye World',0
+GREETING_STRING:
+    db 'Starting boot process in 16-bit real mode',0
+
 
 
 ;padding with 510 - size of code so far - zeroes and magic number to ID as bootsector code
 times 510 - ($-$$) db 0
 dw 0xaa55
+
+
+;putting in false data to be read
+times 256 dw 0xdada
+times 256 dw 0xfaa3
